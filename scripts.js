22d7de8a5f298dd127cd9d1e7892e2ab416bfeab
HK Kahng <hk@router-rooter.com>
2016-12-12 21:16:27 -0800

    add initial API call code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
-alert("Halloo!");
+var discogsToken = "";
+
+var artist = document.getElementById("input_artist");
+var track = document.getElementById("input_track");
+var button = document.getElementById("user_button");
+var output = document.getElementById("output_list");
+
+button.addEventListener("click", function() {
+  if (artist.value === "" && track.value === "") {
+    alert("Hey, you can't search with an empty query!");
+    return;
+  };
+  output.innerHTML = "";
+  var queryArray = [];
+  artist.value != "" ? queryArray.push("artist=" + artist.value) : "";
+  track.value != "" ? queryArray.push("track=" + track.value) : "";
+  var queryURL = "https://api.discogs.com/database/search?type=master&" + queryArray.join('&') + "&token=" +  discogsToken;
+
+  var discogsCall = new XMLHttpRequest();
+  discogsCall.addEventListener("load", processCall);
+  discogsCall.open("GET", queryURL);
+  discogsCall.send();
+
+  function processCall() {
+    var returnData = JSON.parse(discogsCall.responseText);
+    for (var i = 0; i < returnData.results.length; i++) {
+      console.log(returnData.results[i]);
+      var listItem = document.createElement("LI");
+      var listLabelText = "<a href='http://www.discogs.com" + returnData.results[i].uri + "'>" + returnData.results[i].title + "</a>";
+      listItem.setAttribute("class", "list-group-item");
+      listItem.innerHTML = listLabelText;
+      output.appendChild(listItem);
+    }
+  }
+
+});
